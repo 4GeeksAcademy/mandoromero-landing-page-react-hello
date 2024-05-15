89c5005220701a648b5cdb10f4b68c2e35806beb
Armando Romero <162987396+mandoromero@users.noreply.github.com>
2024-05-15 14:39:40 +0000

    chages

--- src/js/component/myjumbotron.jsx
+++ src/js/component/myjumbotron.jsx
@@ -3,13 +3,13 @@ import React from "react"
 const Jumbotron = () => {
     return (
 
-        <div style={{backgroundColor: "#e9ecef"}}class="jumbotron m-3 p-3 mb-2 text-dark">
-  <h1 class="display-4">A Warm Welcome!</h1>
-  <p class="lead">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
-  <p class="lead">
-    <a class="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
-  </p>
-</div>
+        <div style={{ backgroundColor: "#e9ecef" }} className="jumbotron m-5 p-3 mt-0 mb-2 text-dark">
+            <h1 className="display-4">A Warm Welcome!</h1>
+            <p className="lead">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+            <p className="lead">
+                <a className="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
+            </p>
+        </div>
     )
 }
 
--- src/js/component/mynavbar.jsx
+++ src/js/component/mynavbar.jsx
@@ -3,7 +3,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand">Start Bootstrap</a>
+                <a className="navbar-brand pr-3">Start Bootstrap</a>
                 <div className="navbar-nav ml-auto">
                     <a className="nav-link active" aria-current="page" href="#">Home</a>
                     <a className="nav-link" href="#">About</a>
