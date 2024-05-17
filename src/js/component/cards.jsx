9a193bd19718a58387ae6de73e6a6f48d90a50d0
Armando Romero <162987396+mandoromero@users.noreply.github.com>
2024-05-17 14:26:31 +0000

    even better

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -5,10 +5,18 @@ const Cards = () => {
     return(
             
               <div class="row justify-content-center">
-                    <MyCard />
-                    <MyCard />
-                    <MyCard />
-                    <MyCard />
+                    <MyCard
+                        cardImage = "./src/img/OuterSpace.jpg"
+                        cardTitle = "Outer Space"
+                    />
+                    <MyCard 
+                        cardImage = "../OuterSpace"
+                        cardTitle = "The Milky Way"
+                    />
+                    <MyCard 
+                        cardImage = "https://st2.depositphotos.com/4164031/7705/i/450/depositphotos_77053509-stock-photo-planet-earth-in-outer-space.jpg"
+                        cardTitle = "Earth"/>
+                    <MyCard cardTitle = "The Moon"/>
               </div>              
               
     )
--- src/js/component/mycard.jsx
+++ src/js/component/mycard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-    const MyCard = () => {
+    const MyCard = props => {
+        const cardImage = props.cardImage
+        const cardTitle = props.cardTitle
         return(
             <div className="col-sm-3">
-                <img src="https://th.bing.com/th/id/OIP.p7wteZi4gGKn8xf2J7kgrQHaEo?rs=1&pid=ImgDetMain" className="card-img-top" alt="Outer-Space"/>
+                <img src={cardImage} className="card-img-top" alt="Outer-Space"/>
                     <div className="card-body">
-                        <h5 className="card-title">Outer Space</h5>
+                        <h5 className="card-title">{cardTitle}</h5>
                         <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus quis varius quam quisque id diam vel.</p>
                         <br></br>
                         <a href="#" className="btn btn-primary">Find out more!</a>
