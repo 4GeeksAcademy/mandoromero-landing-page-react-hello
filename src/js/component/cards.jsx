8d667cec501ebd57a05f40dd8ebdb72897af4b88
Armando Romero <162987396+mandoromero@users.noreply.github.com>
2024-05-18 05:18:07 +0000

    done

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -4,9 +4,9 @@ import MyCard from "./mycard";
 const Cards = () => {
     return(
             
-              <div class="row justify-content-center">
-                    <MyCard
-                        cardImage = "https://th.bing.com/th/id/R.ac4dd85979c534f9488b2b6c07e54bb6?rik=qtoWw1BWSElPyA&riu=http%3a%2f%2fgetwallpapers.com%2fwallpaper%2ffull%2f6%2fc%2f1%2f564171.jpg&ehk=wXKBBnrv6i9%2fVJ%2bdINC0%2fW%2fNecHKsOcfxOth4ET9DQU%3d&risl=&pid=ImgRaw&r=0"
+              <div className="row justify-content-center">
+                    <MyCard 
+                        cardImage = "https://wallup.net/wp-content/uploads/2018/09/26/670141-space-outer-universe-stars-photography-detail-astronomy-nasa-hubble.jpg"
                         cardTitle = "Outer Space"
                     />
                     <MyCard 
@@ -15,10 +15,12 @@ const Cards = () => {
                     />
                     <MyCard 
                         cardImage = "https://assets.wired.com/photos/w_2400/wp-content/uploads/2014/12/2014bluemarble_2014089_lrg.jpg"
-                        cardTitle = "Earth"/>
+                        cardTitle = "Earth"
+                    />
                     <MyCard
                         cardImage = "https://images.theconversation.com/files/270537/original/file-20190423-175510-ulmp7h.jpg?ixlib=rb-1.1.0&rect=0%2C34%2C1278%2C638&q=45&auto=format&w=1356&h=668&fit=crop"
-                        cardTitle = "The Moon"/>
+                        cardTitle = "The Moon"
+                    />
               </div>              
               
     )
--- src/js/component/mycard.jsx
+++ src/js/component/mycard.jsx
@@ -4,8 +4,8 @@ import React from "react";
         const cardImage = props.cardImage
         const cardTitle = props.cardTitle
         return(
-            <div className="col-sm-3 500x350">
-                <img src={cardImage} className="card-img-top" alt="Outer-Space"/>
+            <div className="col-sm-3">
+                <img src={cardImage} className="card-img-top" alt="Outer-Space" width="300" height="150"/>
                     <div className="card-body">
                         <h5 className="card-title">{cardTitle}</h5>
                         <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus quis varius quam quisque id diam vel.</p>
